eec8614032e64ecfb205ee9def03834c17e87e83
Kevin Gentile <kg168212@ohio.edu>
2016-07-27 20:34:02 -0400

    Revised LAB2 Submission

--- LAB2.asm
+++ LAB2.asm
@@ -1,109 +1,113 @@
-;The following is existing code from the powerpoint slides. NEEDS DEBUGGING :(
-    
-;!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-;FORCED CONFIGURATION ADJUSTMENTS FOR NEWER COMPILER + MPLAB
-;MAY NEED TO ADJUST IN LAB
-    
-#include "p16F87.inc"
+;******************************************************************************************
+;*
+;*  Laboratory # 2
+;*      
+;*   	Program manages the 7-segment display by counting up in 1 second
+;*       	intervals on the leftmost display. The count starts at 0 and ends at 9, 
+;*  	where it then loops back to 0.
+;*
+;*  EE3954 Microprocessors and Microcontrollers
+;*
+;******************************************************************************************
 
-; CONFIG1
-; __config 0xFFFF
- __CONFIG _CONFIG1, _FOSC_EXTRCCLK & _WDTE_ON & _PWRTE_OFF & _MCLRE_ON & _BOREN_ON & _LVP_ON & _CPD_OFF & _WRT_OFF & _CCPMX_RB0 & _CP_OFF
-; CONFIG2
-; __config 0xFFFF
- __CONFIG _CONFIG2, _FCMEN_ON & _IESO_ON
-;!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+list p=16F87
 
-;!!!!!!!!!!!!!!!!!!!!!
-;trouble with tags in newer IDE
-;
- 
-PORTB	equ 0x06    ;tags
-;TRISB	equ 0x06
-PORTD	equ 0x08
-TRISD	equ 0x08
-PLC equ	0x02
-FSR equ	0x04
-INDF	equ 0x00
-STATUS	equ 0x03
-W  equ	d'0'
-F  equ	d'1'
-Z  equ	d'2'
-RP1 equ d'6'
-RP0 equ d'5'
-IRP equ d'7'
+ __CONFIG 0x3F39     ; Code protect OFF, Debug DISABLED, Flash Write ENABLED
+                   	 ; Data EE Protection OFF, Low Voltage Programming DISABLED,
+                   	 ; Brown-out detection DISABLED, Power-up Timer ENABLED,
+                   	 ; Watchdog Timer DISABLED, XT Oscillator selected
 
- ;General Purpouse Register tags
-GROUP1	udata 0x20
-NUMBER	res 1
-in_cnt	res 1
-mid_cnt	res 1
-out_cnt	res 1
-	
-	
+;Initialize special function registers
+PORTB	equ	0x06   
+TRISB	equ	0x06
+PORTD	equ	0x08
+TRISD	equ	0x08
+PCL	equ	0x02
+FSR	equ	0x04
+INDF	equ	0x00
+STATUS	equ	0x03
+
+W	equ	d'0'		; destination = working register
+F	equ	d'1'		; destination = file register
+Z	equ	d'2'		; zero bit in STATUS register
+RP1 equ	d'6'		; bank select high bit in STATUS register
+RP0	equ	d'5'		; bank select low bit in STATUS register
+IRP	equ	d'7'		; indirect addressing select bit in STATUS register
+
+GROUP1	udata 0x20		; User general purpose register tags
+NUMBER	res 1		; the current count to be displayed	
+in_cnt	res 1		; inner delay counter	
+mid_cnt	res 1		; middle delay counter
+out_cnt	res	1		; outer delay counter
+		
+
 	org 0x000
-	
-	bsf STATUS,RP0	;choose bank 1
-	clrf	TRISD	;make port D output
-	movlw b'11100001'   ;make 4,3,2,1 outputs
-	movwf TRISB
-	bcf STATUS,RP0
-	clrf	NUMBER
-	clrf	PORTD
-	clrf	PORTB
-	bsf PORTB,1
-	
-MAIN:	nop
-    movf    NUMBER,W
-    call    NUM
-    movwf   PORTD
-    call    DELAY
-    btfss   PORTB,0x00
-    call    allon
-    
-    incf    NUMBER,F
-    movlw   d'10'
-    subwf   NUMBER,W
-    btfsc   STATUS,Z
-    clrf    NUMBER
-    goto MAIN
-    
-DELAY:	
-    movlw d'167'
-    movwf out_cnt
+INIT:
+	bsf 	STATUS,RP0	;choose bank 1 for memory to set outputs
+	clrf	TRISD		;make port D output
+	movlw 	b'11100001'   	;make bits 4,3,2,1 in 
+	movwf 	TRISB		; PORTB outputs
+	bcf 	STATUS,RP0	; choose bank0 in memory
+	clrf	NUMBER	; initialize NUMBER to 0
+	clrf	PORTD	; initialize all segments off
+	clrf	PORTB	; initialize all digits off
+	bsf 	PORTB,1	; enable right-most digit
+
+MAIN:				
+   	movf    NUMBER,W	; get the next digit to be displayed
+   	call    	NUM		; call num to find the code for the next value
+    movwf 	PORTD	; display the next value
+    call    	DELAY	; call timing loop to delay for one second
+    btfss  	PORTB,0x00	; check if RBO is set, if pressed call allon
+    call   	allon		; turns on all digits
+    incf   	NUMBER,F	; increment NUMBER 
+    movlw   d'10'		; Move 10 into working directory to test for overflow
+    subwf   NUMBER,W	; subtract 10 from NUMBER
+    btfsc   	STATUS,Z	; if zero bit is set, then NUMBER is 10 and
+   	clrf    	NUMBER	; NUMBER needs cleared to start back at 0
+    nop			; else, no op in order to set breakpoint 
+    goto 	MAIN		; repeat main again to display next number
+
+DELAY:
+    movlw 	d'167'		; set the outer counter starting value
+    movwf 	out_cnt		; to 167
+
 mid_agn:
-    movlw	d'176'
-    movwf mid_cnt
+    movlw	d'176'		; set the middle counter starting value
+    movwf	mid_cnt	; to 176
+
 in_agn:
-    movlw d'10'
-    movwf   in_cnt
+   	movlw 	d'10'		; set the inner counter starting value
+   	movwf 	in_cnt		; to 10
+
 in_nxt:
-    decfsz in_cnt,f
-    goto in_nxt
-    decfz mid_cnt,f
-    goto in_agn
-    decfsz  out_cnt,f
-    goto mid_agn
+    decfsz 	in_cnt,f		; decrement inner counter
+    goto 	in_nxt		; if not zero, decrement again
+    decfsz 	mid_cnt,f	; decrement middle counter
+    goto 	in_agn		; if not zero, repeat from in_agn
+    decfsz 	out_cnt,f	; decrement outer counter
+    goto 	mid_agn	; if not zero, repeat from mid_agn
     return
-    
+
 NUM:
-    addwf	PCL,F		; 1
-    retlw	B'00111111'	; 2 - return the code for a '0'
-    retlw	B'00000110'	; 2 - return the code for a '1'
-    retlw	B'01011011'	; 2 - return the code for a '2'
-    retlw	B'01001111'	; 2 - return the code for a '3'
-    retlw	B'01100110'	; 2 - return the code for a '4'
-    retlw	B'01101101'	; 2 - return the code for a '5'
-    retlw	B'01111100'	; 2 - return the code for a '6'
-    retlw	B'00000111'	; 2 - return the code for a '7'
-    retlw	B'01111111'	; 2 - return the code for a '8'
-    retlw	B'01100111'	; 2 - return the code for a '9'
+    addwf	PCL,F		; jump a number of instructions ahead depending on the 
+; current value of the working directory
+    retlw	B'00111111'	; return the code for a '0'
+    retlw	B'00000110'	; return the code for a '1'
+    retlw	B'01011011'	; return the code for a '2'
+    retlw	B'01001111'	; return the code for a '3'
+    retlw	B'01100110'	; return the code for a '4'
+    retlw	B'01101101'	; return the code for a '5'
+    retlw	B'01111100'	; return the code for a '6'
+    retlw	B'00000111'	; return the code for a '7'
+    retlw	B'01111111'	; return the code for a '8'
+    retlw	B'01100111'	; return the code for a '9'
 
 allon:
-    movlw   B'00011110'
-    movwf   PORTB
+   	movlw 	B'00011110'	; enable all digits
+    movwf 	PORTB	;
     return
-    	
+
 end
  
     
